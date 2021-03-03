ce069c4781371646f0725d2f28784d74d17aacfa
ridwan <irfanridwan20@gmail.com>
2021-03-03 19:35:45 +0600

    added read statusin book display,added functionality for toggling that status and made all the form inputs required

--- library.js
+++ library.js
@@ -5,6 +5,14 @@ function Book(title,author,pages,read){
     this.pages=pages
     this.read=read
 }
+Book.prototype.toggleStatus=function(){
+    if (this.read=="read"){
+        this.read="Not Read"
+    }
+    else if(this.read=="Not Read"){
+        this.read="read"
+    }
+}
 let displayDiv=document.querySelector("#display")
 function display(){
     let div=document.createElement("div")
@@ -14,13 +22,29 @@ function display(){
     text2.textContent=myLibrary[a].author
     let text3=document.createElement("p")
     text3.textContent=myLibrary[a].pages
+    let text4=document.createElement("p")
+    text4.textContent=myLibrary[a].read    
+
+    let toggleStatusButton=document.createElement("button") 
+    if (myLibrary[a].read=="read"){
+        toggleStatusButton.textContent="Didn't Read"
+    }
+    else if(myLibrary[a].read=="Not Read") {
+        toggleStatusButton.textContent="read"
+    }
+    toggleStatusButton.classList.add("toggleStatus")
+    toggleStatusButton.setAttribute('data-index',`${myLibrary.indexOf(myLibrary[a])}`)
+    
     let removeButton=document.createElement("button")
     removeButton.textContent="Remove"
     removeButton.classList.add("removeButton")
     removeButton.setAttribute('data-index',`${myLibrary.indexOf(myLibrary[a])}`)
+    
     div.appendChild(text1)
     div.appendChild(text2)
     div.appendChild(text3)
+    div.appendChild(text4)
+    div.appendChild(toggleStatusButton)
     div.appendChild(removeButton)
     div.classList.add("div")
     displayDiv.appendChild(div)
@@ -34,16 +58,39 @@ createButton.addEventListener('click',()=>{
 let a=0;
 let b;
 let submitButton=document.querySelector("#submit")
+let toggleStatusButton;
 let removeButtons;
+let c;
 submitButton.addEventListener('click',()=>{
     /*form.setAttribute('id','form');
     submitButton.style.display="none"*/    
     title=form[0].value;
     author=form[1].value;
     pages=form[2].value;
-    myLibrary[a]=new Book(title,author,pages);
+    if (form[3].checked==true){
+        read=form[3].value;
+    }
+    else if(form[4].checked==true){
+        read=form[4].value;
+    }
+    if (title!='' && author!='' && pages!='' && form[3].checked || form[4].checked){
+    myLibrary[a]=new Book(title,author,pages,read);
     display();
     b=0;
+    
+    toggleStatusButton=document.querySelectorAll(".toggleStatus")
+    toggleStatusButton[a].addEventListener('click',(e)=>{
+        myLibrary[e.target.dataset.index].toggleStatus()
+        c=e.target.parentNode;
+        c.children[3].textContent=`${myLibrary[e.target.dataset.index].read}`;
+        if (myLibrary[e.target.dataset.index].read=="read"){
+            c.children[4].textContent="Didn't Read"
+        }
+        else if(myLibrary[e.target.dataset.index].read=="Not Read") {
+            c.children[4].textContent="Read"
+        }
+    })
+    
     removeButtons=document.querySelectorAll('.removeButton')
     removeButtons[a].addEventListener('click',(e)=>{
         a--;
@@ -52,22 +99,8 @@ submitButton.addEventListener('click',()=>{
         console.log(e.target.parentNode)
         displayDiv.removeChild(e.target.parentNode)
     })
-    a++;
-})
-/*function removeCard(){
-    removeButtons=document.querySelectorAll('.removeButton')
-    for (let remove of removeButtons){       
-        remove.addEventListener('click',()=>{
-        console.log(remove)
-        myLibrary.splice(`${remove.dataset.index}`,1)
-        console.log(`${remove.dataset.index}`)
-        displayDiv.children[`${remove.dataset.index}`].remove();
-        a--;
-        b++;
-    })
-    if (b>0){
-        b=0;
-        break;
+    a++;}
+    else {
+        alert("please properly fill the form!")
     }
-}
-}*/
+})
