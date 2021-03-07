f8aad6d7f086263956d8f60548516ec2ea82556b
ridwan <irfanridwan20@gmail.com>
2021-03-07 11:46:39 +0600

    added a modal,bug fixes and code readability improvement

--- library.js
+++ library.js
@@ -13,49 +13,54 @@ Book.prototype.toggleStatus=function(){
         this.read="read"
     }
 }
-let displayDiv=document.querySelector("#display")
+let displayDiv=document.querySelector("#display");
 function display(){
-    let div=document.createElement("div")
-    let text1=document.createElement("p")
-    text1.textContent=myLibrary[a].title
-    let text2=document.createElement("p")
-    text2.textContent=myLibrary[a].author
-    let text3=document.createElement("p")
-    text3.textContent=myLibrary[a].pages
-    let text4=document.createElement("p")
-    text4.textContent=myLibrary[a].read    
+    let div=document.createElement("div");
+    let text1=document.createElement("p");
+    text1.textContent=myLibrary[a].title;
+    let text2=document.createElement("p");
+    text2.textContent=myLibrary[a].author;
+    let text3=document.createElement("p");
+    text3.textContent=myLibrary[a].pages;
+    let text4=document.createElement("p");
+    text4.textContent=myLibrary[a].read;    
 
-    let toggleStatusButton=document.createElement("button") 
+    let toggleStatusButton=document.createElement("button"); 
     if (myLibrary[a].read=="read"){
-        toggleStatusButton.textContent="Didn't Read"
+        toggleStatusButton.textContent="Didn't Read";
     }
     else if(myLibrary[a].read=="Not Read") {
-        toggleStatusButton.textContent="read"
+        toggleStatusButton.textContent="read";
     }
-    toggleStatusButton.classList.add("toggleStatus")
-    toggleStatusButton.setAttribute('data-index',`${myLibrary.indexOf(myLibrary[a])}`)
+    toggleStatusButton.classList.add("toggleStatus");
+    toggleStatusButton.setAttribute('data-index',`${myLibrary.indexOf(myLibrary[a])}`);
     
-    let removeButton=document.createElement("button")
-    removeButton.textContent="Remove"
-    removeButton.classList.add("removeButton")
-    removeButton.setAttribute('data-index',`${myLibrary.indexOf(myLibrary[a])}`)
+    let removeButton=document.createElement("button");
+    removeButton.textContent="Remove";
+    removeButton.classList.add("removeButton");
+    removeButton.setAttribute('data-index',`${myLibrary.indexOf(myLibrary[a])}`);
     
-    div.appendChild(text1)
-    div.appendChild(text2)
-    div.appendChild(text3)
-    div.appendChild(text4)
-    div.appendChild(toggleStatusButton)
-    div.appendChild(removeButton)
-    div.classList.add("div")
-    displayDiv.appendChild(div)
+    div.appendChild(text1);
+    div.appendChild(text2);
+    div.appendChild(text3);
+    div.appendChild(text4);
+    div.appendChild(toggleStatusButton);
+    div.appendChild(removeButton);
+    div.classList.add("div");
+    displayDiv.appendChild(div);
 }
-let form=document.querySelector("#super")
+
+let addBookButton=document.querySelector("#addBook");
+addBookButton.onclick=()=>{
+    form.style.display="block"
+}
+
+let form=document.querySelector("#form");
 let a=0;
-let b;
-let submitButton=document.querySelector("#submit")
+let submitButton=document.querySelector("#submit");
 let toggleStatusButton;
 let removeButtons;
-let c;
+let updateIndexOfToggleButton;
 submitButton.addEventListener('click',()=>{
     title=`Title-${form[0].value}`;
     author=`Author-${form[1].value}`;
@@ -66,34 +71,38 @@ submitButton.addEventListener('click',()=>{
     else if(form[4].checked==true){
         read=form[4].value;
     }
-    if (title!='' && author!='' && pages!='' && form[3].checked || form[4].checked){
-    myLibrary[a]=new Book(title,author,pages,read);
-    display();
-    b=0;
+    if (title!='Title-' && author!="Author-" && pages!='Pages-' && (form[3].checked || form[4].checked)){
+        myLibrary[a]=new Book(title,author,pages,read);
+        display();
+
+        toggleStatusButton=document.querySelectorAll(".toggleStatus");
+        toggleStatusButton[a].addEventListener('click',(e)=>{
+            myLibrary[e.target.dataset.index].toggleStatus();
+            let c=e.target.parentNode;
+            c.children[3].textContent=`${myLibrary[e.target.dataset.index].read}`;
+            if (myLibrary[e.target.dataset.index].read=="read"){
+                c.children[4].textContent="Didn't Read";
+            }
+            else if(myLibrary[e.target.dataset.index].read=="Not Read") {
+                c.children[4].textContent="Read";
+            }
+        })
     
-    toggleStatusButton=document.querySelectorAll(".toggleStatus")
-    toggleStatusButton[a].addEventListener('click',(e)=>{
-        myLibrary[e.target.dataset.index].toggleStatus()
-        c=e.target.parentNode;
-        c.children[3].textContent=`${myLibrary[e.target.dataset.index].read}`;
-        if (myLibrary[e.target.dataset.index].read=="read"){
-            c.children[4].textContent="Didn't Read"
-        }
-        else if(myLibrary[e.target.dataset.index].read=="Not Read") {
-            c.children[4].textContent="Read"
-        }
-    })
-    
-    removeButtons=document.querySelectorAll('.removeButton')
-    removeButtons[a].addEventListener('click',(e)=>{
-        a--;
-        myLibrary.splice(`${e.target.dataset.index}`,1)
-        console.log(`${e.target.dataset.index}`)
-        console.log(e.target.parentNode)
-        displayDiv.removeChild(e.target.parentNode)
-    })
-    a++;}
-    else {
-        alert("please properly fill the form!")
+        removeButtons=document.querySelectorAll('.removeButton');
+        removeButtons[a].addEventListener('click',(e)=>{
+            a--;
+            myLibrary.splice(`${e.target.dataset.index}`,1);
+            displayDiv.removeChild(e.target.parentNode);
+            updateIndexOfToggleButton=document.querySelectorAll(".toggleStatus");
+            for(let i=0;i<=a-1;i++){
+                updateIndexOfToggleButton[i].dataset.index=`${myLibrary.indexOf(myLibrary[i])}`;
+            }
+        })
+        a++;
+        form.style.display="none";
     }
 })
+let closeForm=document.querySelector(".close");
+closeForm.onclick=()=>{
+    form.style.display="none";
+}
